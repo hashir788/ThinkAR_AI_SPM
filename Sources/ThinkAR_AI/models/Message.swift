6668ac9ee5d6956fc7d1d4789833a246611630fd
Mohamed Hashir <hashirs788@gmail.com>
2024-08-02 18:06:14 -0700

    Message public initializer added

--- Sources/ThinkAR_AI/models/Message.swift
+++ Sources/ThinkAR_AI/models/Message.swift
@@ -9,6 +9,14 @@ import Foundation
 
 
 public struct Message{
+    
+    public init(id: String, content: String, role: String, createdAt: Date) {
+        self.id = id
+        self.content = content
+        self.role = role
+        self.createdAt = createdAt
+    }
+    
     var id:String;
     var content:String;
     var role:String;
