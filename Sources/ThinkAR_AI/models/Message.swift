09649db3ab64d04efb1dd3f21760585dad01f9db
Mohamed Hashir <hashirs788@gmail.com>
2024-08-02 18:50:01 -0700

    Message init removed

--- Sources/ThinkAR_AI/models/Message.swift
+++ Sources/ThinkAR_AI/models/Message.swift
@@ -9,14 +9,6 @@ import Foundation
 
 
 public struct Message{
-    
-    public init(id: String, content: String, role: String, createdAt: Date) {
-        self.id = id
-        self.content = content
-        self.role = role
-        self.createdAt = createdAt
-    }
-    
     var id:String;
     var content:String;
     var role:String;
