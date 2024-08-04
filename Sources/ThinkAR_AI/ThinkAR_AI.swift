4ec5549905c663bec439c32e3a5f0c4f7a72655f
Mohamed Hashir <hashirs788@gmail.com>
2024-08-04 22:12:25 +0530

    Groq Fix

--- Sources/ThinkAR_AI/ThinkAR_AI.swift
+++ Sources/ThinkAR_AI/ThinkAR_AI.swift
@@ -8,7 +8,7 @@ public struct ThinkAR_AI{
     private static var  groqKey = "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"
 //    private let service:some OpenAIService = OpenAIServiceFactory.service(apiKey: .bearer(groqKey), baseURL:"https://api.groq.com/openai")
     
-    private let service:some OpenAIService = OpenAIServiceFactory.service( baseURL:"https://api.groq.com/openai/openai/")
+    private let service:some OpenAIService = OpenAIServiceFactory.service( baseURL:"https://api.groq.com//openai")
     
     public init(){}
     
